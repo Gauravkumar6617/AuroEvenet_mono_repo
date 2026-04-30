479685a0a88f51c36d2da3c070c34caeccaa18e9
Gaurav Kumar <gauravkumar94527@gmail.com>
2026-04-30 22:44:41 +0530

    fix in vite env

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,7 +2,11 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
-  // more env variables...
+  readonly VITE_INTERNAL_API_KEY: string
+  readonly VITE_GOOGLE_CLIENT_ID: string
+  readonly VITE_GOOGLE_REDIRECT_URI: string
+  readonly VITE_GITHUB_CLIENT_ID: string
+  readonly VITE_GITHUB_REDIRECT_URI: string
 }
 
 interface ImportMeta {
