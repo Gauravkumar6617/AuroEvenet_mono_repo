d7b9177160dc1d18959e66fb0e822c70f015805d
Gaurav Kumar <gauravkumar94527@gmail.com>
2026-04-30 07:17:55 +0530

    to fix unwanted user issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,6 @@ const App: React.FC = () => {
               <Navbar />
               <main className="flex-grow container mx-auto px-4">
                 <Routes>
-
                   <Route path="/" element={<Home />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/signup" element={<Signup />} />
